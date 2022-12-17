f6a1b82a055eb448acffd602d2daa9e275b897aa
Ленар <@DESKTOP-MRQMG2R>
2022-12-17 12:44:44 +0500

    Добавьте файлы проекта.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public static Entry[] Inputs = new Entry[]
+        {
+            new Entry()
+            {
+                Placeholder = "Введите коэфицент a",
+                BackgroundColor = Color.White,
+                TextColor = Color.Black,
+                FontSize = 20,
+                Margin = new Thickness(20, 0, 20, 10),
+                HorizontalOptions = LayoutOptions.Fill,
+                Keyboard = Keyboard.Numeric,
+                ClearButtonVisibility = ClearButtonVisibility.WhileEditing
+            },
+
+            new Entry()
+            {
+                Placeholder = "Введите коэфицент b",
+                BackgroundColor = Color.White,
+                TextColor = Color.Black,
+                FontSize = 20,
+                Margin = new Thickness(20, 0, 20, 10),
+                HorizontalOptions = LayoutOptions.Fill,
+                Keyboard = Keyboard.Numeric,
+                ClearButtonVisibility = ClearButtonVisibility.WhileEditing
+            },
+
+            new Entry()
+            {
+                Placeholder = "Введите коэфицент c",
+                BackgroundColor = Color.White,
+                TextColor = Color.Black,
+                FontSize = 20,
+                Margin = new Thickness(20, 0, 20, 10),
+                HorizontalOptions = LayoutOptions.Fill,
+                Keyboard = Keyboard.Numeric,
+                ClearButtonVisibility = ClearButtonVisibility.WhileEditing
+            }
+        };
+
+        private void PrintError()
+        {
+            for (int i = 0; i < Inputs.Length; i++)
+            {
+                if (string.IsNullOrEmpty(Inputs[i].Text))
+                {
+                    Inputs[i].Text = null;
+                    Inputs[i].PlaceholderColor = Color.Red;
+                    Inputs[i].Placeholder = "Обязательное поле";
+                }
+
+                else if (Inputs[i].Text[0] == '0')
+                {
+                    Inputs[i].Text = null;
+                    Inputs[i].PlaceholderColor = Color.Red;
+                    Inputs[i].Placeholder = "Некоректные данные";
+                }
+            }
+        }
+        
+        private async void ResultButtonClicked(object sender, EventArgs e)
+        {
+
+            bool hasInvalid = Inputs.Any(input => string.IsNullOrEmpty(input.Text) || input.Text[0] == '0');
+
+            if (hasInvalid)
+            {
+                PrintError();
+                return;
+            }
+
+            await Navigation.PushAsync(new ResultPage
+            {
+                Coeficents = Inputs
+            });
+        }
+        protected override void OnAppearing()
+        {
+            StackLayout multipliers = new StackLayout()
+            {
+                BackgroundColor = Color.White
+            };
+
+            Label[] Labels = new Label[]
+            {
+                new Label()
+                {
+                    Text = "Коэфицент a:",
+                    TextColor = Color.Black,
+                    FontSize = 25,
+                    Margin = new Thickness (22, 0, 20, 5),
+                    HorizontalOptions = LayoutOptions.Fill
+                },
+
+                new Label()
+                {
+                    Text = "Коэфицент b:",
+                    TextColor = Color.Black,
+                    FontSize = 25,
+                    Margin = new Thickness(22, 30, 20, 5),
+                    HorizontalOptions = LayoutOptions.Fill
+                },
+
+                new Label()
+                {
+                    Text = "Коэфицент c:",
+                    TextColor = Color.Black,
+                    FontSize = 25,
+                    Margin = new Thickness(22, 30, 20, 5),
+                    HorizontalOptions = LayoutOptions.Fill
+                }
+            };
+
+            
+
+            for (int i = 0 ; i < Labels.Length && i < Inputs.Length; i++)
+            {
+                multipliers.Children.Add(Labels[i]);
+                multipliers.Children.Add(Inputs[i]);
+            }
+
+            Button ResultButton = new Button()
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                Text = "Вычислить",
+                TextColor = Color.Black,
+                Margin = new Thickness(20, 0, 20, 0),
+                FontSize = 15
+            };
+
+            ResultButton.Clicked += ResultButtonClicked;
+
+            multipliers.Children.Add(ResultButton);
+
+            Content = multipliers;
+        }
+    }
+}
---
+++ App1/App1/ResultPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResultPage : ContentPage
+    {
+        public Entry[] Coeficents = new Entry[3];
+
+        public ResultPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            Title = "Результаты";
+
+            StackLayout Results = new StackLayout()
+            {
+                BackgroundColor = Color.White
+            };
+
+            double a = Convert.ToDouble(Coeficents[0].Text);
+            double b = Convert.ToDouble(Coeficents[1].Text);
+            double c = Convert.ToDouble(Coeficents[2].Text);
+            double discriminant = Math.Pow(b, 2) - 4 * a * c;
+
+            Label Discriminant = new Label()
+            {
+                Text = $"Дискриминант: {discriminant}",
+                TextColor = Color.Black,
+                FontSize = 30,
+                Margin = new Thickness(22, 35, 20, 10),
+                HorizontalOptions = LayoutOptions.Fill
+            };
+            Results.Children.Add(Discriminant);
+
+            Label NoEquationRoots = new Label()
+            {
+                Text = "Нет корней уравнения",
+                TextColor = Color.Black,
+                FontSize = 30,
+                Margin = new Thickness(22, 20, 20, 5),
+                HorizontalOptions = LayoutOptions.Fill
+            };
+
+            Label OneEquationRoot = new Label()
+            {
+                Text = $"x = {-b / (2 * a)}",
+                TextColor = Color.Black,
+                FontSize = 30,
+                Margin = new Thickness(22, 10, 20, 5),
+                HorizontalOptions = LayoutOptions.Fill
+            };
+
+            Label FirstEquationRoot = new Label()
+            {
+                Text = $"x1 = {(-b + Math.Sqrt(discriminant)) / (2 * a)}\n",
+                TextColor = Color.Black,
+                FontSize = 30,
+                Margin = new Thickness(22, 20, 20, 0),
+                HorizontalOptions = LayoutOptions.Fill
+            };
+
+            Label SecondEquationRoot = new Label()
+            {
+                Text = $"x2 = {(-b - Math.Sqrt(discriminant)) / (2 * a)}",
+                TextColor = Color.Black,
+                FontSize = 30,
+                Margin = new Thickness(22, -25, 20, 5),
+                HorizontalOptions = LayoutOptions.Fill
+            };
+
+            if (discriminant < 0)
+            {
+                Discriminant.Text += " < 0";
+                Results.Children.Add(NoEquationRoots);
+            }
+            if (discriminant == 0)
+            {
+                Discriminant.Text += " = 0";
+                Results.Children.Add(OneEquationRoot);
+            }
+            if (discriminant > 0)
+            {
+                Discriminant.Text += " > 0";
+                Results.Children.Add(FirstEquationRoot);
+                Results.Children.Add(SecondEquationRoot);
+            }
+
+            Content = Results;
+        }
+    }
+}
